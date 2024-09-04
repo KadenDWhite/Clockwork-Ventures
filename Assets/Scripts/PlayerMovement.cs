9791ce42e56cd9d65f51cae05bca595be1c7cb77
Kaden W. <kadendw23@gmail.com>
2024-09-04 12:37:39 -0500

    Player Movement
    -  player movement script added for testing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float Move;
+
+    public float speed;
+    public float jump;
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded()) 
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+        }
+    }
+
+
+    public bool isGrounded()
+    {
+        if(Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
+    }
+
+ //   private void OnCollisionEnter2D(Collision2D other) 
+ //   {
+ //       if (other.gameObject.CompareTag("Ground"))
+ //       {
+ //           Vector3 normal = other.GetContact(0).normal;
+ //           if(normal == Vector3.up)
+ //           {
+ //               grounded = true;
+ //           }
+            
+ //       }
+ //   }
+
+ //   private void OnCollisionExit2D(Collision2D other)
+	//{
+	//	if (other.gameObject.CompareTag("Ground"))
+	//	{
+	//		grounded = false;
+	//	}
+	//}
+}
