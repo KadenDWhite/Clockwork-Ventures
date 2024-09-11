3dfb672f6e65c95656d1eee67f534f56099c3f61
K.D.W <kadendw23@gmail.com>
2024-09-11 09:57:09 -0500

    Steps to Attacks

--- Assets/Class Scripts/Weapon.cs
+++ Assets/Class Scripts/Weapon.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Weapon : MonoBehaviour
 {
-     public virtual void Use() {}
+    public UnityEvent equiped;
+    public UnityEvent reload;
+    public virtual void Use() {}
 }
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public List<WeaponManager> weapons;
+    public float swap;
+}
