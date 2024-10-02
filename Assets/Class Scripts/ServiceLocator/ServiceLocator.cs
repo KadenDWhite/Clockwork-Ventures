07d2e280da9238e4e42944aaa57db9acf717bfdd
K.D.W <kadendw23@gmail.com>
2024-10-02 09:20:36 -0500

    Service Locator Class Code

---
+++ Assets/Class Scripts/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour
+{
+    public SLPlayer player;
+    public List<SLEnemy> enemies;
+    public SLAudioManager audioManager;
+}
