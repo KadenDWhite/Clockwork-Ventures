18ace29764f3f7b667167580bcda2432054e4eeb
Kaden W. <kadendw23@gmail.com>
2024-11-07 18:50:50 -0600

    Script and scene changes

--- Assets/Scripts/Scene Managers/MainMenu.cs
+++ Assets/Scripts/Scene Managers/MainMenu.cs
@@ -1,12 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor; // Allows using SceneAsset in the Editor
 
 public class MainMenu : MonoBehaviour
 {
-    public List<SceneAsset> scenesToLoad; // Drag your scenes here in the Inspector
+    public List<string> sceneNames; // List of scene names
 
     public void PlayGame()
     {
@@ -21,10 +19,10 @@ public class MainMenu : MonoBehaviour
     // Load a scene based on its index in the list
     public void LoadLevelByIndex(int index)
     {
-        if (index >= 0 && index < scenesToLoad.Count)
+        if (index >= 0 && index < sceneNames.Count)
         {
-            // Load the scene using the name of the SceneAsset
-            string sceneName = scenesToLoad[index].name;
+            // Load the scene using the scene name
+            string sceneName = sceneNames[index];
             SceneManager.LoadScene(sceneName);
         }
         else
@@ -36,15 +34,14 @@ public class MainMenu : MonoBehaviour
     // Load a scene based on its name
     public void LoadLevelByName(string sceneName)
     {
-        foreach (SceneAsset scene in scenesToLoad)
+        if (sceneNames.Contains(sceneName))
         {
-            if (scene.name == sceneName)
-            {
-                SceneManager.LoadScene(scene.name);
-                return;
-            }
+            SceneManager.LoadScene(sceneName);
         }
-        Debug.LogWarning("Scene not found in the list.");
+        else
+        {
+            Debug.LogWarning("Scene not found in the list.");
+        }
     }
 
     public void QuitGame()
